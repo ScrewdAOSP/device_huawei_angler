4f9a813deea79c4c817ada4be48e0222fa877a9d
sdroyds <mikew8978@gmail.com>
2016-01-18 07:46:41 -0600

    Enable MirrorCast Support

--- device.mk
+++ device.mk
@@ -283,6 +283,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.iwlan.enable=true
 
+# Miracast support
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.debug.wfd.enable=1
+
 PRODUCT_PROPERTY_OVERRIDES += \
    ro.frp.pst=/dev/block/platform/soc.0/f9824900.sdhci/by-name/frp
 
