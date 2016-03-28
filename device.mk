c3f29027fba150a2a736fb8027d8ff6e7423665a
f100cleveland <dustin.rinne@gmail.com>
2016-03-28 15:44:23 -0500

    Let the CPU idle down to 302400

--- device.mk
+++ device.mk
@@ -243,8 +243,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # for perfd
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.min_freq_0=384000
-    ro.min_freq_4=384000
+    ro.min_freq_0=302400
+    ro.min_freq_4=302400
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so
