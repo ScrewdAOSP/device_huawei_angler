d1fb72efa2604054dbc7cc3dbfe5783553ae74bf
Kyler <beenreinvented@gmail.com>
2016-04-27 10:14:18 -0700

    Fix redundant facelock entries
    I noticed the spacing is different so idk if that means something or not 😂

--- device.mk
+++ device.mk
@@ -400,15 +400,6 @@ drm.service.enabled=true
 
 # facelock properties
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.facelock.black_timeout=400 \
-ro.facelock.det_timeout=1500 \
-ro.facelock.rec_timeout=2500 \
-ro.facelock.lively_timeout=2500 \
-ro.facelock.est_max_time=600 \
-ro.facelock.use_intro_anim=false
-
-# facelock properties
-PRODUCT_PROPERTY_OVERRIDES += \
     ro.facelock.black_timeout=400 \
     ro.facelock.det_timeout=1500 \
     ro.facelock.rec_timeout=2500 \
